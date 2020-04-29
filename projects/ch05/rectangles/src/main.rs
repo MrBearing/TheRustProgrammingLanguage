90ebd2ae779484de46e919e1f596e68ef3b0c8cf
Takumi Okamoto <takumi1988okamoto@gmail.com>
2020-04-30 01:39:22 +0900

    #3 fin [構造体を使用したプログラム例]

--- projects/ch05/rectangles/src/main.rs
+++ projects/ch05/rectangles/src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+fn area_with_tuple(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
+
+
+fn area_with_size(width: u32, height: u32) -> u32 {
+    width * height
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.height * self.width
+    }
+}
+
+
+fn main() {
+    let width1 = 30;
+    let height1 = 50;
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area_with_size(width1, height1)
+    );
+
+
+    let rect1 = (30, 50);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area_with_tuple(rect1)
+    );
+
+    let struct_rect = Rectangle{width: 30, height: 30};
+    println!("rect1 is {:?}", struct_rect);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        struct_rect.area()
+    );
+
+}
+
