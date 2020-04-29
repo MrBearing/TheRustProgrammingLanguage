171555bbafff24677ec01fb7993eb4ee666cafdf
Takumi Okamoto <takumi1988okamoto@gmail.com>
2020-04-30 01:48:08 +0900

    close #3

--- projects/ch05/rectangles/src/main.rs
+++ projects/ch05/rectangles/src/main.rs
@@ -17,6 +17,17 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.height * self.width
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+// implブロックは複数に分割可能
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
+    }
 }
 
 
@@ -29,7 +40,6 @@ fn main() {
         area_with_size(width1, height1)
     );
 
-
     let rect1 = (30, 50);
 
     println!(
@@ -37,12 +47,23 @@ fn main() {
         area_with_tuple(rect1)
     );
 
+    // after here is 5.3's
     let struct_rect = Rectangle{width: 30, height: 30};
     println!("rect1 is {:?}", struct_rect);
     println!(
         "The area of the rectangle is {} square pixels.",
         struct_rect.area()
     );
+
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+
+    // rect1にrect2ははまり込む？
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    println!("new rect = {:?}",Rectangle::square(32));
 
 }
 
