eb3ef89f2bf16ea47177f7b66d174752938554c8
Takumi Okamoto <takumi1988okamoto@gmail.com>
2020-05-01 23:26:41 +0900

    close #4 ちょっと追加

--- projects/ch06/pattern_match/src/main.rs
+++ projects/ch06/pattern_match/src/main.rs
@@ -18,16 +18,19 @@ fn value_in_cents(coin: Coin) -> u32 {
 }
 
 
-fn plus_one(x: Option<i32>) -> Option<i32> {
-    match x {
-        None => None,
-        Some(i) => Some(i + 1),
-    }
-}
+// fn plus_one(x: Option<i32>) -> Option<i32> {
+//     match x {
+//         None => None,
+//         Some(i) => Some(i + 1),
+//     }
+// }
 
 
 fn main() {
     println!("Hello, world! {}",value_in_cents(Coin::Penny));
+    println!("Hello, world! {}",value_in_cents(Coin::Dime));
+    println!("Hello, world! {}",value_in_cents(Coin::Nickel));
+    println!("Hello, world! {}",value_in_cents(Coin::Quarter));
 
     // let five = Some(5);
     // let six = plus_one(five);
