180ea4e636047254a5f29febb7721f0599a37284
Takumi Okamoto <takumi1988okamoto@gmail.com>
2020-05-01 22:47:46 +0900

    #4 ch06 fin

---
+++ projects/ch06/pattern_match/src/main.rs
@@ -0,0 +1,42 @@
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky Penny");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+
+fn main() {
+    println!("Hello, world! {}",value_in_cents(Coin::Penny));
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    let some_u8_value = Some(0u8);
+    match some_u8_value {
+        Some(3) => println!("three"),
+        _ => (),
+    }
+
+}
