990c2162619971117b290c9bea941ed7d39dfcf2
Takumi Okamoto <takumi1988okamoto@gmail.com>
2020-05-19 00:28:34 +0900

    #19  ch 20.2読了・実装完了

--- projects/ch20/hello/src/bin/main.rs
+++ projects/ch20/hello/src/bin/main.rs
@@ -1,18 +1,29 @@
+extern crate hello;
+use hello::ThreadPool;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::fs::File;
+use std::thread;
+use std::time::Duration;
 
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
     let mut counter = 0;
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         // 接続が確立しました
         counter+=1;
         println!("counter : {}",counter);
-        handle_connection(stream);
+
+        // thread による実装でもF5アタックくらいは切り抜けられた
+        
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -21,9 +32,13 @@ fn handle_connection(mut stream: TcpStream){
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line , filename) = if buffer.starts_with(get){
         ("HTTP/1.1 200 OK\r\n\r\n","hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
     };
