b0ffe960a896f36eabbf26bc4362d07b814937ca
Takumi Okamoto <takumi1988okamoto@gmail.com>
2020-05-18 19:28:25 +0900

    commit

--- projects/ch19/lifetime/src/main.rs
+++ projects/ch19/lifetime/src/main.rs
@@ -10,5 +10,5 @@ impl<'a> Red for Ball<'a> { }
 fn main() {
     let num = 5;
 
-    let obj = Box::new(Ball { diameter: &num }) as Box<dyn Red>;
+    let _obj = Box::new(Ball { diameter: &num }) as Box<dyn Red>;
 }
