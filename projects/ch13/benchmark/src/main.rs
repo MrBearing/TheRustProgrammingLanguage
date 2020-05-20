18b0c02e48fcc9035b4088a86a2c4621e0303320
Takumi Okamoto <takumi1988okamoto@gmail.com>
2020-05-20 11:55:58 +0900

    ループによる円周率計算

--- projects/ch13/benchmark/src/main.rs
+++ projects/ch13/benchmark/src/main.rs
@@ -1,5 +1,3 @@
-
-
 fn a(n: i32) -> f64 {
     match n {
         0 => 1.0,
@@ -24,19 +22,41 @@ fn p(n: i32) -> f64 {
 fn t(n: i32) -> f64 {
     match n {
         0 => 1.0 /4.0,
-        _ => t(n-1) - (a(n-1) - a(n)).powf(2.0)*p(n-1)
+        _ => t(n-1) - (a(n-1) - a(n)).powi(2)*p(n-1)
     }
 }
 
-fn pi(n: i32)->f64{
+fn pi_recursion(n: i32)->f64{
     (a(n) + b(n)).powf(2.0)/4.0/t(n)
 }
 
-fn main() {
-    for n in 1..10 {
-        let pi = pi(n);
-        println!("{:?}",pi);
+fn pi_loop(d: i32) -> f64{
+    let mut a = 1.0;
+    let mut b = 1.0/2.0_f64.sqrt();
+    let mut t = 1.0/4.0;
+    let mut pn = 1.0;
+    
+    for _ in 1..=d {
+        let an = (a + b) / 2.0;
+        b = (a*b).sqrt();
+        t -= (an -a).powi(2) * pn;
+        pn *= 2.0;
+        a = an;
     }
+    (a + b).powi(2) / (4.0 * t)
+}
+
+fn pi_iter(d: i32) -> f64{
+    
+    0.0
+}
+
+
+fn main() {
+    
+    println!("loop      : {}",pi_loop(23));
+    println!("recursion : {}",pi_recursion(23));
+
 }
 
 
@@ -53,18 +73,47 @@ mod tests {
     
 
     #[test]
-    fn test_pi() {
+    fn test_pi_recursion() {
         print!("0 ,");
-        assert!(assert_diff(2.9142, pi(0), 4));
+        assert!(assert_diff(2.9142, pi_recursion(0), 4));
 
         print!("1 ,");
-        assert!(assert_diff(PI, pi(1), 2));
+        assert!(assert_diff(PI, pi_recursion(1), 2));
         
         print!("2 ,");
-        assert!(assert_diff(PI, pi(2), 7));
+        assert!(assert_diff(PI, pi_recursion(2), 7));
         
         print!("3 ,");
-        assert!(assert_diff(PI, pi(3),15));
+        assert!(assert_diff(PI, pi_recursion(3),15));
+    }
+    #[test]
+    fn test_pi_loop() {
+        print!("0 ,");
+        assert!(assert_diff(2.9142, pi_loop(0), 4));
+
+        print!("1 ,");
+        assert!(assert_diff(PI, pi_loop(1), 2));
+        
+        print!("2 ,");
+        assert!(assert_diff(PI, pi_loop(2), 7));
+        
+        print!("3 ,");
+        assert!(assert_diff(PI, pi_loop(3),15));
+    }
+
+    #[test]
+    fn test_pi_iter() {
+        print!("0 ,");
+        assert!(assert_diff(2.9142, pi_iter(0), 4));
+
+        print!("1 ,");
+        assert!(assert_diff(PI, pi_iter(1), 2));
+        
+        print!("2 ,");
+        assert!(assert_diff(PI, pi_iter(2), 7));
+        
+        print!("3 ,");
+        assert!(assert_diff(PI, pi_iter(3),15));
     }
 
 }
